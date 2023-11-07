c81304eb3050fe3ed97759d1aa20798af802f4e4
Tarantula1337 <tarantulalp@gmail.com>
2023-11-08 00:32:04 +0100

    Steam blackscreen Patch

--- dwm.c
+++ dwm.c
@@ -94,6 +94,7 @@ struct Client {
 	int bw, oldbw;
 	unsigned int tags;
 	int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen;
+  int issteam;
   int fakefullscreen;
 	Client *next;
 	Client *snext;
@@ -298,6 +299,9 @@ applyrules(Client *c)
 	XGetClassHint(dpy, c->win, &ch);
 	class    = ch.res_class ? ch.res_class : broken;
 	instance = ch.res_name  ? ch.res_name  : broken;
+
+  if (strstr(class, "Steam") || strstr(class, "steam_app_"))
+    c->issteam = 1;
 
 	for (i = 0; i < LENGTH(rules); i++) {
 		r = &rules[i];
@@ -599,14 +603,16 @@ configurerequest(XEvent *e)
 			c->bw = ev->border_width;
 		else if (c->isfloating || !selmon->lt[selmon->sellt]->arrange) {
 			m = c->mon;
-			if (ev->value_mask & CWX) {
-				c->oldx = c->x;
-				c->x = m->mx + ev->x;
-			}
-			if (ev->value_mask & CWY) {
-				c->oldy = c->y;
-				c->y = m->my + ev->y;
-			}
+      if (!c->issteam) {
+        if (ev->value_mask & CWX) {
+          c->oldx = c->x;
+          c->x = m->mx + ev->x;
+        }
+        if (ev->value_mask & CWY) {
+          c->oldy = c->y;
+          c->y = m->my + ev->y;
+        }
+      }
 			if (ev->value_mask & CWWidth) {
 				c->oldw = c->w;
 				c->w = ev->width;
@@ -1564,6 +1570,8 @@ setfocus(Client *c)
 			XA_WINDOW, 32, PropModeReplace,
 			(unsigned char *) &(c->win), 1);
 	}
+  if (c->issteam)
+    setclientstate(c, NormalState);
 	sendevent(c, wmatom[WMTakeFocus]);
 }
 
